38e08485d7925d9349310a6b20306e824a7a49be
Svetlin Lyubenov <svetlinlyubenov83@gmail.com>
2025-06-18 12:44:27 +0300

    Initial commit with clean files

---
+++ LaptopInventoryAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LaptopInventoryAPI.Models;
+using System.Collections.Generic;
+
+namespace LaptopInventoryAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Laptop> Laptops { get; set; }
+    }
+}
